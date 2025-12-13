b286fd68a5074ceb473a353b041df6c2c56943f6
horace <horce1024@gmail.com>
2025-12-13 19:54:52 +0800

    first init

---
+++ tool-backend/src/main/java/com/horace/toolbackend/ToolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.horace.toolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolBackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ToolBackendApplication.class, args);
+    }
+
+}
---
+++ tool-backend/src/test/java/com/horace/toolbackend/ToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horace.toolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
